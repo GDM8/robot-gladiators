62ed53673d0a8475d7e1f95c6611cce79fd0f5d7
GDM8 <garcendy@gmail.com>
2022-07-27 22:18:36 -0400

    Battle Sim 01

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,43 @@ var enemyAttack = 12;
 var fight = function() {
     window.alert("Welcome to Robot Gladiators");
 
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+
+// SUBTRACT THE VALUE OF 'playerAttack' FROM THE VALUE OF 'enemyHealth' AND USE THAT RESULT TO UPDATE THE VALUE IN THE 'enemyHealth' VARIABLE
+
+// LOG A RESULTING MESSAGE TO THE CONSOLE SO WE KNOW THAT IT WORKED.
+    console.log(
+        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining. "
+    );
+
+// SUBTRACT THE VALUE OF 'enemyAttack' FROM THE VALUE OF 'playerHealth' AND USE THAT RESULT TO UPDATE THE VALUE IN THE 'playerHealth' VARIABLE
+    playerHealth = playerHealth - enemyAttack;
+    enemyHealth = enemyHealth - playerAttack;
+
+// LOG A RESULTING MESSAGE TO THE CONSOLE SO WE KNOW THAT IT WORKED.
+    console.log(
+        enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " health remaining. "
+    );
+
+    console.log(
+        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining "  
+    );
+// CHECK ENEMY HEALTH
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left. ");
+    }  
+    console.log(enemyName + " attacked " + playerName + " . " + playerName + " now has " + playerHealth + " Health remaining");
+
+// CHECK PLAYER HEALTH
+    if(playerHealth <= 0) {
+        window.alert(playerName + " has died ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left .");
+    }
 };
 
 fight();
