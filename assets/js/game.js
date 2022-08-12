74db4c81cc92c3c362859c9be5636cd8e21e6145
GDM8 <garcendy@gmail.com>
2022-08-11 20:26:27 -0400

    start and end game fucntion

--- assets/js/game.js
+++ assets/js/game.js
@@ -69,16 +69,39 @@ var fight = function(enemyNames) {
   } 
 };
 
-for (var i = 0; i < enemyNames.length; i++){
-  if (playerHealth > 0) {
-    window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+var startGame = function() {
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+      var pickedEnemyName = enemyNames[i];
+      enemyHealth = 50;
+      // debugger;
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert(' You have lost your robot in battle! Game Over');
+      break;
+    }
+    // endGame();
+  };
+
+  var endGame = function() {
+    if(playerHealth > 0) {
+      window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + " .");
+    }
+    else {
+      window.alert("You've lost your robot in battle.")
+    }
+
+    var playAgainConfirm = window.confirm("Would youlike to play again?");
+
+    if (playAgainConfirm) {
+      startGame();
+    }
+    else {
+      window.alert("Thank you for playing Robot Gladiators! Come back soon")
+    }
   }
-  else {
-    window.alert(' You have lost your robot in battle! Game Over');
-    break;
-  }
-}
-   
+};
+// start game upon load up
+startGame();
